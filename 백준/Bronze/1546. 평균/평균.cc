109a9e35551799eb305a34ec7b73f3f8540023bc
siiooin <sssyyy0716@naver.com>
2025-09-20 21:02:12 +0900

    [Bronze I] Title: 평균, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.cc
+++ 백준/Bronze/1546. 평균/평균.cc
@@ -2,45 +2,26 @@
 using namespace std;
 
 int main(){
-    int N;
-    int M = 0;
-    double sum = 0.0;
-    double avg= 0.0;
-    double score[1000] = {};
-    double buffer[1000]= {};
-
+    int N = 0;
     cin >> N;
 
-    for(int i = 0; i < N; i++)
-    {
+    int score[1000];
+    for(int i = 0; i < N; i++){
         cin >> score[i];
     }
 
-    for(int i = 0; i < N; i++)
-    {
-        
-        if(score[i] > M)
-        {
-            M = score[i];
+    long sum = 0;
+    long max =0;
+
+    for(int i = 0; i < N; i++){
+        if(score[i] > max){
+            max = score[i];
         }
-    }   
-
-
-    for(int i = 0; i < N; i++)
-    {
-        buffer[i] = (score[i] / M) * 100;
+        sum = sum + score[i];
     }
 
-
-   for(int i = 0; i < N; i++)
-    {
-        sum += buffer[i];
-        avg = sum / N;
-    }
-    
-    cout << avg;
-
-    return 0;
+    double result = ((sum * 100.0) / max)/ N;
+    cout << result << "\n";
 
 
 }
