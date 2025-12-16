a9f26f4e8ac1bdbab6247a6f4c6b1abb170217c2
siiooin <sssyyy0716@naver.com>
2025-12-16 17:41:23 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+ 
+	int sum = 0;
+	vector<int> v(5);
+ 
+	for (int i = 0; i < 5; i++){
+        cin >> v[i], sum += v[i];
+    }
+    
+	sort(v.begin(), v.end());
+    
+	cout << sum / 5 << '\n' << v[2];
+ 
+	return 0;
+}
