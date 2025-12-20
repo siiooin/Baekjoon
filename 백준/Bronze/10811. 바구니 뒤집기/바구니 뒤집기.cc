d7ebe1ab4a86215701aad434de4110ce020f7284
siiooin <sssyyy0716@naver.com>
2025-12-20 21:10:33 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+        int N, M;
+
+        cin >> N >> M;
+
+        int arr[N + 1];
+
+        for(int i =1; i <=N;i++){
+                arr[i] = i;
+        }
+        
+
+        for(int i=1 ;i <= M;i++){
+                int start, end;
+                cin >> start >> end;
+
+                while(start < end){
+                        swap(arr[start], arr[end]);
+                        start++;
+                        end--;
+                }
+
+        }
+
+        for(int i = 1; i <= N; i++){
+                cout << arr[i] << " ";
+                
+        }
+        
+        return 0;
+}
