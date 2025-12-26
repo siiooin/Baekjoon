1bcf9ec335574125cc11c2bfdf55a4ca0df0a0b3
siiooin <sssyyy0716@naver.com>
2025-12-27 07:48:25 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B;
+
+    cin >> A >> B;
+
+    cout << A + B<< "\n";
+    cout << A - B<<"\n";
+    cout << A * B<<"\n";
+    cout << A / B<<"\n";
+    cout << A % B<<"\n";
+
+    return 0;
+
+}
