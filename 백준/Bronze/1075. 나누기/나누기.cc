156449a80d03245dd6e7043bb6bc62b9be701318
siiooin <sssyyy0716@naver.com>
2025-12-30 15:49:07 +0900

    [Bronze II] Title: 나누기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N, F;
+	int answer = 0;
+	cin >> N >> F;
+	
+	N /= 100;
+	N *= 100;
+	
+	while((N+answer) % F != 0 && answer < 100) answer ++;
+	
+	if(answer < 10) printf("0%d", answer);
+	else printf("%d", answer);
+	
+}
