036dfb0b01fc352a3c10055598e6ac543ecde576
siiooin <sssyyy0716@naver.com>
2025-12-30 15:52:01 +0900

    [Bronze II] Title: 나누기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1075. 나누기/나누기.cc
+++ 백준/Bronze/1075. 나누기/나누기.cc
@@ -3,15 +3,18 @@ using namespace std;
 
 int main() {
 	int N, F;
-	int answer = 0;
+	int ans = 0;
 	cin >> N >> F;
 	
 	N /= 100;
 	N *= 100;
 	
-	while((N+answer) % F != 0 && answer < 100) answer ++;
-	
-	if(answer < 10) printf("0%d", answer);
-	else printf("%d", answer);
+	while((N+ ans) % F != 0 && ans < 100){
+        ans++;
+    }
+	if(ans< 10){
+        cout << "0"<< ans;
+    } 
+	else cout <<ans;
 	
 }
