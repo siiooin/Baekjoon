f43f679ff5aa4b4246b570941333eadb42100066
siiooin <sssyyy0716@naver.com>
2026-01-16 21:01:57 +0900

    [Silver V] Title: 단어 정렬, Time: 68 ms, Memory: 4108 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool cmp(string a, string b) {
+	if (a.length() == b.length()) {
+		return a < b;
+	}
+	else {
+		return a.length() < b.length();
+	}
+}
+
+string str[20000];
+
+int main() {
+	int N;
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> str[i];
+	}
+
+	sort(str, str + N, cmp);
+
+	for (int i = 0; i < N; i++) {
+		if (str[i] == str[i - 1]) {
+			continue;
+		}
+		cout << str[i] << "\n";
+	}
+
+	return 0;
+}
+
