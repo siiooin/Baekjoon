b62513489cd37190de217091a68316d6daa5cecc
siiooin <sssyyy0716@naver.com>
2026-02-14 23:01:23 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+  int arr[3];
+
+  for (int i = 0; i < 3; i++) {
+    cin >> arr[i];
+  }
+
+  sort(arr, arr + 3);
+
+  for (int i = 0; i < 3; i++) cout << arr[i] << " ";
+
+  return 0;
+}
